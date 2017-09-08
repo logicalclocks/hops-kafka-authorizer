7abbc1589ae78bd3699713e1732d6a5534f94996
Theofilos Kakantousis <t.kakantousis@gmail.com>
2017-09-08 17:32:50 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+set -e
+git pull
 
-scp target/hops-kafka-authorizer-0.1.jar    glassfish@snurran.sics.se:/var/www/hops
+mvn clean install
+VERSION=`grep -o -a -m 1 -h -r "version>.*</version" ./pom.xml | head -1 | sed "s/version//g" | sed "s/>//" | sed "s/<\///g"`
+echo ""
+echo "Deploying hops-kafka-authorizer-${VERSION}.jar to snurran.sics.se"
+echo ""
+
+scp target/hops-kafka-authorizer-${VERSION}.jar    glassfish@snurran.sics.se:/var/www/hops
